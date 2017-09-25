f08242def324ebf0de4e74a6ba3c1edc2ab05a48
Vladimir Ivanin <ivaninvv@mail.ru>
2017-09-26 01:13:41 +0300

    Update patchNumber.js

--- js/patchNumber.js
+++ js/patchNumber.js
@@ -3,23 +3,23 @@
 * => 11.950000000000001
 * patchNumber(2.39 * 5)
 * => 11.95
-* patchNumber ('5,0')
+* patchNumber('5,0')
 * => 5
-* patchNumber ('5,1')
+* patchNumber('5,1')
 * => 5.1
-* patchNumber ('5,11')
+* patchNumber('5,11')
 * => 5.11
-* patchNumber ('5,111')
+* patchNumber('5,111')
 * => 5.11
-* patchNumber (undefined)
+* patchNumber(undefined)
 * => 0
-* patchNumber ([])
+* patchNumber([])
 * => 0
-* patchNumber ({})
+* patchNumber({})
 * => 0
-* patchNumber (null)
+* patchNumber(null)
 * => 0
-* patchNumber (`asd`)
+* patchNumber(`asd`)
 * => 1
 */
 
@@ -39,5 +39,6 @@ function patchNumber (num) {
   function isFloat(n){
     return Number(n) === n && n % 1 !== 0;
   }
+  
   return Number( (isFloat(num)) ? num.toFixed(2) : num );
 }
