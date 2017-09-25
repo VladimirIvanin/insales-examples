6f7f160eb800c45fa703a985f62863e6a54880c1
Vladimir Ivanin <ivaninvv@mail.ru>
2017-09-26 01:12:56 +0300

    Update patchNumber.js

--- js/patchNumber.js
+++ js/patchNumber.js
@@ -11,16 +11,33 @@
 * => 5.11
 * patchNumber ('5,111')
 * => 5.11
+* patchNumber (undefined)
+* => 0
+* patchNumber ([])
+* => 0
+* patchNumber ({})
+* => 0
+* patchNumber (null)
+* => 0
+* patchNumber (`asd`)
+* => 1
 */
 
-function patchNumber (num){
+function patchNumber (num) {
   var isString = typeof num == 'string';
+  var isNumber = typeof num == 'number';
+  
+  if (!isNumber && !isString) {
+    return 0;
+  }
+
   if(isString){
     num = num.replace(/,/g, '.');
+    num = (isNaN(+num)) ? 1 : +num;
   }
-  var thisNums = (isNaN(+num)) ? 1 : +num;
+
   function isFloat(n){
     return Number(n) === n && n % 1 !== 0;
   }
-  return Number( (isFloat(thisNums)) ? thisNums.toFixed(2) : thisNums );
+  return Number( (isFloat(num)) ? num.toFixed(2) : num );
 }
