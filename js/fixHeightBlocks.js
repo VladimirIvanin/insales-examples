86bd94411155702f6816de224b0fd6903fb36915
Vladimir Ivanin <ivaninvv@mail.ru>
2017-10-20 14:11:43 +0300

    Update fixHeightBlocks.js

--- js/fixHeightBlocks.js
+++ js/fixHeightBlocks.js
@@ -1,12 +1,10 @@
 /**
  * Выравнивание блоков по высоте
  * @param  {string} selector      блоки которые нужно выровнять
- * @param  {string/boolean} innerSelector внутренний блок, если указан то свойство добавиться этому блоку, если выставить false, то значение подставляется блоку указанному в 'selector'
+ * @param  {string} innerSelector внутренний блок
  * @param  {string} _cssValue     css свойство для подстановки значения
  *
   fixHeightBlocks('.card', '.card-title', 'padding-bottom')
-  // or
-  fixHeightBlocks('.card', false, 'padding-bottom')
 
    $(window).on('resize', function(event) {
      fixHeightBlocks('.card', '.card-title', 'padding-bottom')
@@ -40,17 +38,43 @@ function fixHeightBlocks(selector, innerSelector, _cssValue) {
   $.each(groups, function(index, group) {
     var isMax = group.isMax;
     var $blocks = group.items;
+    var _margin = 0;
     $.each($blocks, function(index, el) {
-      var _margin = isMax - $(el).outerHeight();
       var $item = $(el);
       if (innerSelector) {
         $item = $(el).find(innerSelector);
       }
-      if (_margin <= 0) {
-        _margin = $(el).css(cssValue) || 0;
+      var delta = patchNumber($item.css(cssValue));
+      var mainHeight = $item.outerHeight() - delta;
+      _margin = isMax - mainHeight;
+
+      if (_margin < 0) {
+        _margin = 0;
       }
 
       $item.css(cssValue, _margin);
+
     });
   });
+
+  function patchNumber (num) {
+    var isString = typeof num == 'string';
+    var isNumber = typeof num == 'number';
+
+    if (!isNumber && !isString) {
+      return 0;
+    }
+
+    if(isString){
+      num = num.replace(/,/g, '.');
+      num = num.replace(/px/g, '');
+      num = (isNaN(+num)) ? 1 : +num;
+    }
+
+    function isFloat(n){
+      return Number(n) === n && n % 1 !== 0;
+    }
+
+    return Number( (isFloat(num)) ? num.toFixed(2) : num );
+  }
 }
