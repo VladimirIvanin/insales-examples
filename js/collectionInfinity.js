51625c3a1835ac5d2801d46ed7294d78ca4f7307
Vladimir Ivanin <ivaninvv@mail.ru>
2017-12-06 16:57:38 +0300

    Create collectionInfinity.js

---
+++ js/collectionInfinity.js
@@ -0,0 +1,47 @@
+/**
+* data-collection-infinity="{{ paginate.next.url }}"
+* атрибут стоит на родителе карточек коллекции
+*/
+
+if ($('[data-collection-infinity]').length) {
+  // DetectiveScroll это просто обработчик скролла
+  var html = document.documentElement;
+  $(window).on('scroll', function(event) {
+    var scrollTop = html.scrollTop || body && body.scrollTop || 0;
+    scrollTop -= html.clientTop;
+      
+    var collscroll = $('[data-collection-infinity]').get(0).offsetTop + ($('[data-collection-infinity]').outerHeight() / 1.7);
+    
+    if (scrollTop > collscroll) {
+      collectionInfinity();
+    }
+  });
+}
+
+var tempPage = [];
+function collectionInfinity() {
+  var _nextPage = $('[data-collection-infinity]').data('collection-infinity');
+  if (_nextPage && _nextPage != '') {
+    if (tempPage.indexOf(_nextPage) > -1) {
+      return;
+    }
+    tempPage.push(_nextPage);
+    $('body').addClass('body--loading');
+    
+    $.ajax({
+      url: _nextPage,
+      dataType: 'html'
+    })
+    .done(function(_dom) {
+      var $dom = $(_dom);
+      var $next = $dom.find('[data-collection-infinity]');
+      var _next = $next.data('collection-infinity');
+      $('[data-collection-infinity]').append( $next.html() );
+      $('[data-collection-infinity]').data('collection-infinity', _next).attr('data-collection-infinity', _next);
+      
+    })
+    .always(function () {
+      $('body').removeClass('body--loading');
+    })
+  }
+}
